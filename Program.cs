6651b9ea41b9444de13256a26fb9aa553a7168e9
Zakhar <info@zspirin.ru>
2024-02-21 20:20:12 +0300

    Task1

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+void Main()
+{
+
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        string Enter = Console.ReadLine();
+        if (Enter == "q")
+        {
+            break;
+
+        }
+        int num;
+        if (int.TryParse(Enter, out num))
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum % 2 == 0)
+            {
+                System.Console.WriteLine("STOP");
+                break;
+            }
+
+        }
+        else
+        {
+            System.Console.WriteLine("Try again");
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+Main();
